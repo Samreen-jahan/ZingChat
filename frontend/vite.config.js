9bc12fbba7ff83a055bd7b770ca6fe8851c88bc3
unknown <samreenjahan627@gmail.com>
2025-03-14 22:50:30 +0530

    chaanges in vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
   server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-      },
-    },
-  },
-  build: {
-    outDir: "dist", 
-  },
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
 })
